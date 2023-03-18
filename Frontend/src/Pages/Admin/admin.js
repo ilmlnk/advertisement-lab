f2e133c4bd87eb51387f3cee9ccdcd068e2dc0a6
ilmlnk <ilmlnkcorp@gmail.com>
2023-03-18 22:02:26 +0100

    added advertisement view models

--- Frontend/src/Pages/Admin/admin.js
+++ Frontend/src/Pages/Admin/admin.js
@@ -16,7 +16,7 @@ const Admin = () => {
             <div className='admin-content'>
                 <h1>Orders</h1>
                 <div>
-
+                    
                 </div>
 
                 <button>Create new order</button>
@@ -24,8 +24,7 @@ const Admin = () => {
                 <div>
                     <h1>Recent Activity</h1>
                     <span 
-                    onClick={() => setIsOpenUserModal(true)}
-                    >
+                    onClick={() => setIsOpenUserModal(true)}>
                         See all
                     </span>
 

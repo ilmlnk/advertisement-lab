6c82b0df2d275827be6bad19f2713ce2d5548ab3
ilmlnk <ilmlnkcorp@gmail.com>
2023-03-19 20:07:47 +0100

    changed design

--- Frontend/src/Components/footer/Footer.js
+++ Frontend/src/Components/footer/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import './FooterStyle.css';
 
 const Footer = () => {
-
+    return (
+        <footer className="footer">
+            <div>
+                <h2>AdReach</h2>
+            </div>
+        </footer>
+    );
 }
 
 
